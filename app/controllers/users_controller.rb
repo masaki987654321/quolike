d8f1d7f30c1f288607e2765b4f33ec481afa6ee5
EC2 Default User <ec2-user@ip-172-31-44-62.us-east-2.compute.internal>
2019-10-09 00:46:02 +0000

    complete sign-up

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  
+  def show
+    @user = User.find_by(params[:id])
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user =User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  def destroy
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+  
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
     
   root 'home_pages#home'
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  resources :users
 
 end
