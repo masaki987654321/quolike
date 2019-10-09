50a28864d8d8a23d3dd702a3ebaf98fc8fc93499
EC2 Default User <ec2-user@ip-172-31-44-62.us-east-2.compute.internal>
2019-10-09 07:39:52 +0000

    complete-login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
     
+  get 'sessions/new'
+
   root 'home_pages#home'
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   resources :users
 
 end
