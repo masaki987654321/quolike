3edbf546021883af905fcd8889a67507562318e5
kaneko <bull.31415@gmail.com>
2020-08-18 08:06:09 +0900

    viewの修正
    headerの色変更
    footerの削除
    home_pages
    - 色
    - tag検索を削除

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -1,11 +1,7 @@
 class HomePagesController < ApplicationController
   def home
+    # todo questionsにuserをjoinする
     @users = User.all
     @questions = Question.all.paginate(page: params[:page], per_page: 10)
-    
-    if params[:tag_name]
-      @questions = @questions.tagged_with("#{params[:tag_name]}")
-                             .paginate(page: params[:page], per_page: 10)
-    end
   end
 end
