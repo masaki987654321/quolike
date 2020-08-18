f135f42164ed2949e8c8c23c650890e03197eb9b
kaneko <bull.31415@gmail.com>
2020-08-18 12:51:29 +0900

    home_pages_controllerの修正

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -1,7 +1,26 @@
 class HomePagesController < ApplicationController
   def home
-    # todo questionsにuserをjoinする
-    @users = User.all
-    @questions = Question.all.paginate(page: params[:page], per_page: 10)
+    # todo pagenation機能の実装
+    solved_questions = Question.where.not(best_answer_id: nil).joins(:user).select("questions.*, users.name").paginate(page: params[:page], per_page: 10)
+    solved_answers = Question.where.not(best_answer_id: nil).joins(:answers).select("answers.*")
+    
+    @send_questions = []
+    
+    solved_questions.each do |question|
+      answer_counts = question.get_answer_count(solved_answers)
+      best_answer = question.get_best_answer(solved_answers)
+
+      send_question = {
+        id: question.id,
+        content: question.content,
+        user_id: question.user_id,
+        updated_at: question.updated_at,
+        user_name: question.name,
+        answer_counts: answer_counts,
+        best_answer: best_answer
+      }
+
+      @send_questions.push(send_question)
+    end
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -5,4 +5,26 @@ class Question < ApplicationRecord
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 200 }
   acts_as_taggable
+
+  # 回答数取得の処理
+  def get_answer_count(answers)
+    answer_counts = 0
+    answers.each do |answer|
+      if(answer.id == self.best_answer_id)
+        answer_counts += 1
+      end
+    end
+    return answer_counts
+  end
+
+  # ベストアンサー取得の処理
+  def get_best_answer(answers)
+    best_answer = ''
+    answers.each do |answer|
+      if(answer.id == self.best_answer_id)
+        best_answer = answer.content
+      end
+    end
+    return best_answer
+  end
 end
